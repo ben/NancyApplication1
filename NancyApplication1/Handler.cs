2ff434ea0e68755b822d07dd165d38df623e862e
Ben Straub <ben@straub.cc>
2015-05-16 10:34:50 -0700

    POST path hello world

--- NancyApplication1/Handler.cs
+++ NancyApplication1/Handler.cs
@@ -7,14 +7,20 @@ using System.Web;
 
 namespace NancyApplication1
 {
-    public class Class1 : NancyModule
+    public class Handler : NancyModule
     {
-        public Class1()
+        public Handler()
         {
-            Get["/", true] = async (x,ct) => {
+            Get["/", true] = async (x, ct) =>
+            {
                 var client = new GitHubClient(new ProductHeaderValue("MyHello"));
                 var user = await client.User.Get("ben");
                 return String.Format("{0} people love {1}!", user.Followers, user.Name);
+            };
+
+            Post["/{user}/{repo}/{sha}/{status}"] = (parms) =>
+            {
+                return String.Format("/{0}/{1}/{2}/{3}", parms.user, parms.repo, parms.sha, parms.status);
             };
         }
     }
