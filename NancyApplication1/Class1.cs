d0998085a511b555f371b059c09b75f5ff90cc98
Ben Straub <ben@straub.cc>
2015-05-16 10:15:41 -0700

    Nancy hello world

---
+++ NancyApplication1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NancyApplication1
+{
+    public class Class1 : NancyModule
+    {
+        public Class1()
+        {
+            Get["/"] = _ => "Hello, world!";
+        }
+    }
+}
